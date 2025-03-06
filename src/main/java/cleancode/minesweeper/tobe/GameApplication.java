01a1207aa2ed6f762b16ad77713166b3d8f8b294
5jeong <wjdgns4385@naver.com>
2025-03-06 21:01:25 +0900

    ♻️ [refactor] LSP 리팩토링

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,13 +1,14 @@
 package cleancode.minesweeper.tobe;
 
 import cleancode.minesweeper.tobe.gameLevel.Advanced;
+import cleancode.minesweeper.tobe.gameLevel.Beginner;
 import cleancode.minesweeper.tobe.gameLevel.GameLevel;
 
 public class GameApplication {
 
     public static void main(String[] args) {
 
-        GameLevel gameLevel = new Advanced();
+        GameLevel gameLevel = new Beginner();
         MineSweeper mineSweeper = new MineSweeper(gameLevel);
         mineSweeper.run();
     }
