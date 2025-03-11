fcb1e44f4d3588d3e5a6b3942bc8434d3d89035c
5jeong <wjdgns4385@naver.com>
2025-03-12 00:19:37 +0900

    ♻️ [refactor] 추상화 수준 통일 (중복로직 개선)

--- src/main/java/cleancode/mission/day7/model/StudyCafePassType.java
+++ src/main/java/cleancode/mission/day7/model/StudyCafePassType.java
@@ -2,20 +2,26 @@ package cleancode.mission.day7.model;
 
 public enum StudyCafePassType {
 
-    HOURLY("시간 단위 이용권", "1"),
-    WEEKLY("주 단위 이용권", "2"),
-    FIXED("1인 고정석", "3");
+    HOURLY("시간 단위 이용권", "1", "%d시간권 - %d원"),
+    WEEKLY("주 단위 이용권", "2", "%d주권 - %d원"),
+    FIXED("1인 고정석", "3", "%d주권 - %d원");
 
     private final String description;
     private final String number;
+    private final String format;
 
-    StudyCafePassType(String description, String number) {
+    StudyCafePassType(String description, String number, String format) {
         this.description = description;
         this.number = number;
+        this.format = format;
     }
 
     public String getNumber() {
         return number;
+    }
+
+    public String format(int duration,int price){
+        return format.formatted(duration,price);
     }
 
 }
