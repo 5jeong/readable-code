13f60c76eebdf4984794fdcba66c90b348ddb90d
5jeong <wjdgns4385@naver.com>
2025-03-12 00:05:15 +0900

    ♻️ [refactor] import제거

--- src/main/java/cleancode/mission/day7/model/StudyCafePassType.java
+++ src/main/java/cleancode/mission/day7/model/StudyCafePassType.java
@@ -1,17 +1,15 @@
 package cleancode.mission.day7.model;
 
-import cleancode.mission.day7.exception.AppException;
-
 public enum StudyCafePassType {
 
-    HOURLY("시간 단위 이용권","1"),
-    WEEKLY("주 단위 이용권","2" ),
-    FIXED("1인 고정석","3" );
+    HOURLY("시간 단위 이용권", "1"),
+    WEEKLY("주 단위 이용권", "2"),
+    FIXED("1인 고정석", "3");
 
     private final String description;
     private final String number;
 
-    StudyCafePassType( String description, String number) {
+    StudyCafePassType(String description, String number) {
         this.description = description;
         this.number = number;
     }
